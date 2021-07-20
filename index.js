8fde3b22f210d4bd075ff2ac4017e5772c02e3aa
joey <zhaoyi0113@gmail.com>
2021-07-20 17:39:42 +1000

    fix typo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const hello = (name) => `Hello: ${name}`;
 
-const frineds = ['Michael', 'Joey', 'Alex', 'Dave'];
+const friends = ['Michael', 'Joey', 'Alex', 'Dave'];
 
-frineds.forEach((f) => console.log(hello(f)));
+friends.forEach((f) => console.log(hello(f)));
